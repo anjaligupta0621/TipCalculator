772eace8e5c5a39f98bbb614c5a9fd57c5afbcae
Anjali Gupta <anjaligupta0621@gmail.com>
2020-08-16 23:34:57 +0530

    init_TipCalculator

---
+++ calc.js
@@ -0,0 +1,12 @@
+var amount_paid;
+var rating_given;
+var num_people;
+
+function calculateTip() {
+    var tip;
+    amount_paid = document.getElementById("amount").value;
+    rating_given = document.getElementById("ratings").value;
+    num_people = document.getElementById("people").value;
+    tip = (rating_given*amount_paid)/num_people;
+    document.getElementById("tipamount").innerHTML = "TIP AMOUNT" + '<br>' + '$ '+ tip + '<br>each';
+}
