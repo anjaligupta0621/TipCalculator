04ce94e6fe982198dc157460287e3b8bdcbd8cbf
Anjali Gupta <anjaligupta0621@gmail.com>
2020-08-17 14:31:13 +0530

    design_updated

--- calc.js
+++ calc.js
@@ -7,6 +7,13 @@ function calculateTip() {
     amount_paid = document.getElementById("amount").value;
     rating_given = document.getElementById("ratings").value;
     num_people = document.getElementById("people").value;
-    tip = (rating_given*amount_paid)/num_people;
-    document.getElementById("tipamount").innerHTML = "TIP AMOUNT" + '<br>' + '$ '+ tip + '<br>each';
+
+    if (amount_paid === '') {
+        alert("Please enter the total bill amount!");
+    } else if (num_people === '') {
+        alert("Please enter the number of people");
+    } else {
+        tip = (rating_given*amount_paid)/num_people;
+        document.getElementById("tipamount").innerHTML = "TIP AMOUNT" + '<br>' + '$ '+ tip + '<br>each';
+    }
 }
